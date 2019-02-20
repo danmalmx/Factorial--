682b58df4d498856a8014631adf15caf341612ee
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 13:57:40 +0100

    adding 'classical' for loop method as solution

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -10,6 +10,22 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Enter the nth value as an integer:" );
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Loop calculation: Factorial {n} is {CalculateLooop(n)}");
+            Console.ReadKey();
         }
+            private static int CalculateLooop(int n)
+            {
+                var factorial = 1;
+                for (int i = n; i >= 1; i--)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+
+            }
     }
 }
