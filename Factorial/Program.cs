3db28dae01ee5c1232b6f3f5bade47d3c01c9b2e
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 14:11:15 +0100

    addign recursive method

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -15,17 +15,26 @@ namespace Factorial
             var n = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"Loop calculation: Factorial {n} is {CalculateLooop(n)}");
+            Console.WriteLine($"Recusrive calculation: Factorial {n} is {CalculateRecursion(n)}");
+
             Console.ReadKey();
         }
-            private static int CalculateLooop(int n)
+        private static int CalculateLooop(int n)
+        {
+            var factorial = 1;
+            for (int i = n; i >= 1; i--)
             {
-                var factorial = 1;
-                for (int i = n; i >= 1; i--)
-                {
-                    factorial *= i;
-                }
-                return factorial;
-
+                factorial *= i;
             }
+            return factorial;
+
+        }
+
+        private static int CalculateRecursion(int n)
+        {
+            if (n == 1)
+                return 1;
+            return n * CalculateRecursion(n - 1);
+        }
     }
 }
